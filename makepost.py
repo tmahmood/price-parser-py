9197bf69ecbdbea82cc7e833fff2cf5ff54de189
Tarin Mahmood <mahmood.tarin@gmail.com>
2016-02-26 00:01:34 +0600

    Small changes

--- makepost.py
+++ makepost.py
@@ -6,6 +6,26 @@ import requests
 import shutil
 import sys
 
+XTITLE = "id('productTitle')"
+XPRICE = '//div[@class="bem-product-price__unit--pdp"]'
+XRATING = '//div[@class="bem-review-stars__wrapper"]'
+XDESC = '//div[@itemprop="description"]'
+XFEAT = '//div[@class="bem-content"]/dl[1]/dd'
+XIMG = '//a[@class="zoomable-image"]/img'
+XIMG2 = '//div[@id="mainImageWrapper"]/img'
+
+
+def get_dom():
+    """
+    get dom from html
+    """
+    if len(sys.argv) < 2:
+        print("missing url to parse")
+        sys.exit(1)
+    url = sys.argv[1]
+    resp = requests.get(url)
+    return url, html.fromstring(resp.content)
+
 
 def download_image(url):
     """downloads an image
@@ -19,7 +39,7 @@ def download_image(url):
     del response
 
 
-def get_elm(xpath, index=None):
+def get_elm(dom, xpath, index=None):
     """returns element by xpath
 
     """
@@ -29,64 +49,85 @@ def get_elm(xpath, index=None):
     return elm
 
 
-def get_text(xpath, index):
+def get_text(dom, xpath, index):
     """returns text of element by index
 
     """
-    elm = get_elm(xpath, index)
+    elm = get_elm(dom, xpath, index)
     return elm.text_content().strip()
 
-rate = 130
 
-if len(sys.argv) < 2:
-    print("missing url to parse")
-    sys.exit(1)
-url = sys.argv[1]
-resp = requests.get(url)
-dom = html.fromstring(resp.content)
-data = {}
-data['title'] = get_text("id('productTitle')", 0)
-price = float(get_text('//div[@class="bem-product-price__unit--pdp"]', 0)[1:])
-data['price'] = price * rate
-rating = get_elm('//div[@class="bem-review-stars__wrapper"]', 0)
-data['rating'] = rating.attrib['title'].strip().replace('Star review', '')
-data['description'] = get_text('//div[@itemprop="description"]', 0)
-features = dom.xpath('//div[@class="bem-content"]/dl[1]/dd')
-txt = []
-for feature in features:
-    txt.append(feature.text_content())
-data['features'] = '\n»    '.join(txt).strip()
-try:
-    data['img'] = get_elm('//a[@class="zoomable-image"]/img', 0).attrib['src']
-except Exception as e:
-    data['img'] = get_elm('//div[@id="mainImageWrapper"]/img', 0).attrib['src']
-data['url'] = url
-data['review_url'] = data['url'] + '#tabCustReviews'
-if data['img'].startswith('//'):
-    data['img'] = 'http:' + data['img']
-download_image(data['img'])
-if len(sys.argv) >= 3:
-    data['weight_charge'] = (int(sys.argv[2]) / 1000) * 600
-    data['total_price'] = data['weight_charge'] + data['price']
-else:
-    data['total_price'] = '%s taka BUT NO WEIGHT PROVIDED' % data['price']
+def main():
+    """
+    Main
+    """
+    rate = 130
+    weight_charge = 600
+    url, dom = get_dom()
+    data = {}
+    data['title'] = get_text(dom, XTITLE, 0)
+    price = float(get_text(dom, XPRICE, 0)[1:])
+    data['price'] = price * rate
+    rating = get_elm(dom, XRATING, 0)
+    data['rating'] = rating.attrib['title'].strip().replace('Star review', '')
+    data['description'] = get_text(dom, XDESC, 0)
+    features = dom.xpath(XFEAT)
+    txt = []
+    for feature in features:
+        txt.append(feature.text_content())
+    data['features'] = '\n»    '.join(txt).strip()
+    data['url'] = url
+    data['review_url'] = data['url'] + '#tabCustReviews'
+    failed = False
+    try:
+        data['img'] = get_elm(dom, XIMG, 0).attrib['src']
+    except:
+        failed = True
+    if failed:
+        try:
+            data['img'] = get_elm(dom, XIMG2, 0).attrib['src']
+        except:
+            print("Failed to download image")
+            data['img'] = ''
+    if data['img'].startswith('//'):
+        data['img'] = 'http:' + data['img']
+    if data['img'] != '':
+        download_image(data['img'])
+    if len(sys.argv) >= 3:
+        weight = int(sys.argv[2])
+        data['weight'] = weight
+        data['weight_charge'] = (weight / 1000) * weight_charge
+        data['total_price'] = data['weight_charge'] + data['price']
+    else:
+        data['total_price'] = '%s taka BUT NO WEIGHT PROVIDED' % data['price']
+    with open('post_content', 'w') as fp:
+        template = """%(title)s
 
-with open('post_content', 'w') as fp:
-    tpl = """%(title)s
-
-Price:   %(total_price)s
+--------------------------------------------
+Price
+%(price)s
+--
+Est. Weight
+%(weight)s g
+--
+Price inc. weight charge
+%(total_price)s
+--------------------------------------------
+Rating : %(rating)s
+--------------------------------------------
 Details: %(url)s
-Review:  %(review_url)s
-Rating:  %(rating)s
-
+Review : %(review_url)s
+--------------------------------------------
 Description:
 %(description)s
-
+--------------------------------------------
 Features:
 »    %(features)s
-
-
         """
-    txt = tpl % data
-    fp.write(txt)
-    print (txt)
+        txt = template % data
+        fp.write(txt)
+        print(txt)
+
+
+if __name__ == '__main__':
+    main()
